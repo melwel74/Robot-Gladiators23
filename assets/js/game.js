f73e08cc5f2f4359bd2717652aae7daa9f295d6c
Melvin Welton <nivlem65@gmail.com>
2022-12-06 09:07:18 -0600

    refactoring js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,21 +1,28 @@
-var fight = function() {
+var fight = function () {
   // Alert players that they are starting the round
   window.alert("Welcome to Robot Gladiators!");
 
+  
+  
   //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
   enemyHealth = enemyHealth - playerAttack;
   // Log a resulting message to the console so we know that it worked.
   console.log(
-    playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    playerName + " attacked " + enemyNames + ". " + enemyNames + " now has " + enemyHealth + " health remaining."
   )
   // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
-
+  enemyAttack = enemyAttack - playerHealth;
   // Log a resulting message to the console so we know that it worked.
+  console.log(
+    enemyNames + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+  )
 };
 
 
 // var playerName = 'Clank McKrank';
 var playerName = window.prompt("What is your robot's name?");
+window.alert(playerName);
+
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
@@ -26,7 +33,8 @@ console.log(playerName, playerAttack, playerHealth);
 
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 console.log(enemyNames.length);
-for(var i = 0; i < enemyNames.length; i++) {
+
+for (var i = 0; i < enemyNames.length; i++) {
   console.log(enemyNames[i]);
   console.log(enemyNames[i] + " is at " + i + " index")
 }
@@ -34,8 +42,8 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 // fight function
-var fight = function(enemyName) {
-  while(enemyHealth > 0) {
+var fight = function (enemyName) {
+  while (enemyHealth > 0) {
     // place fight function code block here . . .
   }
   // Alert players that they are starting the round
@@ -92,9 +100,48 @@ var fight = function(enemyName) {
   }
 };
 
-for(var i = 0; i < enemyNames.length; i++) {
+for (var i = 0; i < enemyNames.length; i++) {
   fight(enemyNames[i]);
 }
 
 // run fight function to start game
 fight();
+
+// function to start a new game
+var startGame = function () {
+  // reset player stats
+  playerHealth = 100;
+  playerAttack = 10;
+  playerMoney = 10;
+
+  for (var i = 0; i < enemyNames.length; i++) {
+    if (playerHealth > 0) {
+      // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
+      window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+
+      // pick new enemy to fight based on the index of the enemyNames array
+      var pickedEnemyName = enemyNames[i];
+
+      // reset enemyHealth before starting new fight
+      enemyHealth = 50;
+
+      // use debugger to pause script from running and check what's going on at that moment in the code
+      // debugger;
+
+      // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
+      fight(pickedEnemyName);
+    }
+    else {
+      window.alert("You have lost your robot in battle! Game Over!");
+      break;
+    }
+  }
+  // play again
+  startGame();
+}
+
+function smoke(){
+  window.alert("You have smoked a BLUNT!");
+  var promptSmoke = window.prompt("Would you like to smoke another BLUNT? Enter 'YES' or 'NO' to choose.");
+}
+smoke();
